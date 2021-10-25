7a141908e9f2b8e00d2a55b17cf6f40f950df75d
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2021-10-25 13:15:39 +0200

    4.3 ajout des components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {PagesModule} from "./pages/pages.module";
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
