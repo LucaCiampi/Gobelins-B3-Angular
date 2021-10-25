553a80735533cbe9f20cc418bf2d09ab11719c1d
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2021-10-25 11:16:34 +0200

    création bouton retour accueil

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,36 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './login/login.component';
-import { PasswordForgetComponent } from './password-forget/password-forget.component';
-import { SignupComponent } from './signup/signup.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {LoginComponent} from './login/login.component';
+import {PasswordForgetComponent} from './password-forget/password-forget.component';
+import {SignupComponent} from './signup/signup.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'home-page',
-        pathMatch: 'full'
-    },
-     {
-        path: 'home-page',
-        component: HomePageComponent
-    },
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'password_forget',
-        component: PasswordForgetComponent
-    },
-    {
-        path: 'signup',
-        component: SignupComponent
-    },
+  {
+    path: '', // Chemin par défaut http://localhost:4200 ou serveur renvoie sur /home-page
+    redirectTo: 'home-page',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home-page', // correspond à http://localhost:4200/home-page
+    component: HomePageComponent
+  },
+  {
+    path: 'login', // correspond à http://localhost:4200/login
+    component: LoginComponent
+  },
+  {
+    path: 'password_forget',
+    component: PasswordForgetComponent // correspond à http://localhost:4200/password_forget
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {
+}
