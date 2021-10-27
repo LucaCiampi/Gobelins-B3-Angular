3f60d29020a72cf83c8959ec35830e42622065f3
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2021-10-27 10:29:27 +0200

    8. Fonction setInterval on title

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'home-page', //selecteur html pour appeler le compo
@@ -12,8 +13,14 @@ export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
 
   // variables et functions convention de nomage camelCase
   currentSection = 'home-page';
+  timerInterval: any;
+  titleTextData = [
+    'Create amazing landing page with Gobelins',
+    'Perfect solution for small businesses',
+    'We love make things amazing and simple'
+  ]
 
-  constructor() {
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {
     // exécuté en premier !
 
     // exemple de log à checker dans la console du navigateur
@@ -24,39 +31,49 @@ export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
     // exécuté automatiquement à l'initialisation du composant
     // obligatoire si implements OnInit (qui est facultatif)
     // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
-
   }
 
   ngAfterViewInit() {
     // exécuté automatiquement après l'initialisation de la vue
     // obligatoire si implements AfterViewInit (qui est facultatif)
     // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
+
+    const title = document.getElementById('text');
+    if (!title) return;
+
+    let i = 1;
+    this.timerInterval = setInterval(() => {
+      title.innerHTML = this.titleTextData[i];
+      i++;
+      if (i >= this.titleTextData.length) {
+        i = 0;
+        // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+      }
+    }, 2500);
   }
 
   // Toujours utiliser public ou private pour les fonctions, variables , constantes ...
   // Si pas précisé, c'est public par défaut mais on s'expose à des soucis de lint
   // NOTE : Pour les appels depuis le html, mettre en public
   public windowScroll(): void {
+    const html = document.getElementById('navbar');
+    if (!html) return;
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-      // @ts-ignore
-      document.getElementById('navbar').style.backgroundColor = '#1a1a1a';
-      // @ts-ignore
-      document.getElementById('navbar').style.padding = '15px 0px';
+      html.style.backgroundColor = '#1a1a1a';
+      html.style.padding = '15px 0px';
     } else {
-      // @ts-ignore
-      document.getElementById('navbar').style.backgroundColor = '';
-      // @ts-ignore
-      document.getElementById('navbar').style.padding = '20px';
+      html.style.backgroundColor = '';
+      html.style.padding = '20px';
     }
   }
 
- // Utiliser void si la fonction ne renvoie rien
-  private returnNothing (): void {
+  // Utiliser void si la fonction ne renvoie rien
+  private returnNothing(): void {
     const fake = this.returnABoolean(false)
   }
 
   // Préciser le type ( boolean, any, unknown ...)
-  private returnABoolean (input : boolean): boolean {
+  private returnABoolean(input: boolean): boolean {
     const fake = !input;
     return fake;
   }
@@ -76,6 +93,8 @@ export class HomePageComponent implements OnInit, AfterViewInit, OnDestroy {
     // obligatoire si implements OnDestroy (qui est facultatif)
     // Important : Si on a souscrit à des abonnements, se désabonner ici pour éviter les fuites de mémoires
     // D'autres existent, voir https://angular.io/guide/lifecycle-hooks
+
+    clearInterval(this.timerInterval);
   }
 
 }
