6f1b2fecbf66c1fdafc02ac6a49c146b69bde06a
LucaCiampi <luca.ciampi@edu.gobelins.fr>
2021-11-15 18:49:28 +0100

    model 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {PagesModule} from "./pages/pages.module";
-import { ExcerptPipe } from './pipes/excerpt.pipe';
 
 @NgModule({
   declarations: [
--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -1,23 +1,23 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {CommonModule} from '@angular/common';
-import {CarouselModule} from 'ngx-owl-carousel-o';
-import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
+import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
-import {ClientComponent} from './client/client.component';
-import {SwitcherComponent} from './switcher/switcher.component';
-import {ScrollspyDirective} from './scrollspy.directive';
-import {FooterComponent} from './footer/footer.component';
+import { ClientComponent } from './client/client.component';
+import { SwitcherComponent } from './switcher/switcher.component';
+import { ScrollspyDirective } from './scrollspy.directive';
+import { FooterComponent } from './footer/footer.component';
 import { ServicesComponent } from './services/services.component';
 import { TeamComponent } from './team/team.component';
 import { FaqComponent } from './faq/faq.component';
 import { CtaComponent } from './cta/cta.component';
 import { FeaturesComponent } from './features/features.component';
-import {PricingComponent} from "./pricing/pricing.component";
-import {ContactComponent} from "./contact/contact.component";
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
 import { ExternalLinksComponent } from './team/external-links/external-links.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {ExcerptPipe} from "../pipes/excerpt.pipe";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ExcerptPipe } from '../pipes/excerpt.pipe';
 
 @NgModule({
   declarations: [
@@ -33,15 +33,15 @@ import {ExcerptPipe} from "../pipes/excerpt.pipe";
     PricingComponent,
     ContactComponent,
     ExternalLinksComponent,
-    ExcerptPipe
+    ExcerptPipe,
   ],
-    imports: [
-        CommonModule,
-        CarouselModule,
-        ScrollToModule.forRoot(),
-        FormsModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    CommonModule,
+    CarouselModule,
+    ScrollToModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [
     ClientComponent,
     SwitcherComponent,
@@ -55,8 +55,7 @@ import {ExcerptPipe} from "../pipes/excerpt.pipe";
     PricingComponent,
     ContactComponent,
     ExternalLinksComponent,
-    ExcerptPipe
-  ]
+    ExcerptPipe,
+  ],
 })
-export class SharedModule {
-}
+export class SharedModule {}
